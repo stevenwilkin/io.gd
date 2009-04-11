ab0665a1e2fbc80ec694aa3a7cec3b7ec5784915
Steven Wilkin <stevenwilkin@gmail.com>
2009-04-11 19:44:03 +0100

    basics of url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,16 @@
 class Url < ActiveRecord::Base
+
+  private
+
+  # test existance of a url by connecting to it
+  def valid_url(url)
+    require 'open-uri'
+    begin
+      open url
+    rescue
+      return false
+    end
+    true
+  end
+
 end
