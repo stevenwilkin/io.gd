edc3ed9a9fd79458044d9ee23a4131b84e3d2c2e
Steven Wilkin <stevenwilkin@gmail.com>
2009-04-12 14:20:20 +0100

    hopefully schema for urls table finalised

--- db/migrate/20090410204713_create_urls.rb
+++ db/migrate/20090410204713_create_urls.rb
@@ -1,9 +1,10 @@
 class CreateUrls < ActiveRecord::Migration
   def self.up
     create_table :urls do |t|
-      t.string :short_url, :null => false
       t.string :url, :null => false
     end
+    # need case-sensitivity for short_url column
+    execute "ALTER TABLE urls ADD COLUMN short_url VARBINARY(5) NOT NULL AFTER id"
     add_index :urls, :short_url, :unique => true
   end
 
