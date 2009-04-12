33f657a8bbe6c1764c03aee1eb85a7c8a08a71c4
Steven Wilkin <stevenwilkin@gmail.com>
2009-04-12 16:46:13 +0100

    get latest short url created

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,19 @@
 class Url < ActiveRecord::Base
 
+  # the latest short_url created
+  def self.last_short_url
+    sql = <<-END_SQL
+      SELECT
+        *
+      FROM
+        urls
+      ORDER BY
+        LENGTH(short_url) DESC, short_url DESC
+      LIMIT 1
+    END_SQL
+    find_by_sql(sql)
+  end
+
   private
 
   # test existance of a url by connecting to it
