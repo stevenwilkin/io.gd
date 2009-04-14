1d84e49ccfdb8584519127d456057891f418b797
Steven Wilkin <stevenwilkin@gmail.com>
2009-04-15 00:26:04 +0100

    select contents on shortened url field when clicked

--- public/javascripts/io.js
+++ public/javascripts/io.js
@@ -4,5 +4,10 @@
  */
 
 $(document).ready(function(){
+  // auto-focus url input field
   $('#url_url').focus();
+  // select contents of shortened url field when clicked
+  $('#short_url').click(function(){
+    $(this).select(); 
+  });
 });
