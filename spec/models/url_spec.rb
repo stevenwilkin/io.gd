028f25e0cfece536c6800ad4bfbf6ed88196fa55
Steven Wilkin <stevenwilkin@gmail.com>
2009-11-02 23:35:08 +0000

    tests for base62 <-> decimal conversion

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 describe Url do
  
+  context "when generating an identifier for a new shortened url" do
+
+    it "should convert decimal to base62" do
+      Url.convert_to_base62(42692).should eql('B6a')
+    end
+
+    it "should convert the base62 to decimal" do
+      Url.convert_from_base62('eZe').should eql(155970)
+    end
+
+  end
+
   context "when creating a new shortened url" do
 
     it "should be valid with a supplied url"  do
