b954409c8c278b88b510963ca7dfacdb2cdf5520
Steven Wilkin <stevenwilkin@gmail.com>
2014-08-28 22:40:48 +0100

    Use Postgres instead of MySQL

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '1.8.7'
 
 gem 'rails', '2.3.4'
 
-gem 'mysql'
+gem 'pg'
 
 group :development, :test do
   gem 'rake', '0.8.7'
--- db/migrate/20090410204713_create_urls.rb
+++ db/migrate/20090410204713_create_urls.rb
@@ -2,9 +2,9 @@ class CreateUrls < ActiveRecord::Migration
   def self.up
     create_table :urls do |t|
       t.string :url, :null => false
+      t.binary :short_url
     end
-    # need case-sensitivity for short_url column
-    execute "ALTER TABLE urls ADD COLUMN short_url VARBINARY(5) NOT NULL AFTER id"
+
     add_index :urls, :short_url, :unique => true
   end
 
